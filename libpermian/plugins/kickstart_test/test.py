ff2d5442d142a0e6a3c42fc2bcd287d2ec2c1499
Radek Vykydal <rvykydal@redhat.com>
2022-03-02 14:00:32 +0100

    Add support for installation urls configuration via event

--- libpermian/plugins/kickstart_test/test.py
+++ libpermian/plugins/kickstart_test/test.py
@@ -2,10 +2,13 @@ import unittest
 import os
 import copy
 import shutil
+from textwrap import dedent
+
 
 from libpermian.testruns import TestRuns
 from libpermian.events.base import Event
 from libpermian.settings import Settings
+from libpermian.plugins.kickstart_test import InstallationUrlsStructure
 
 from tclib.library import Library
 
@@ -41,8 +44,16 @@ class TestFakePOCEvent(Event):
         )
 
 
-class TestFakeMultiplatformEvent(Event):
+class TestFakeMultiplatformEvent(TestFakePOCEvent):
     def __init__(self, settings, event_type='kstest-multiplatform'):
+        super().__init__(
+            settings,
+            event_type,
+        )
+
+
+class TestFakeInstallationUrlsEvent(TestFakePOCEvent):
+    def __init__(self, settings, event_type='kstest-urls'):
         super().__init__(
             settings,
             event_type,
@@ -172,3 +183,140 @@ class TestKickstartTestWorkflowResultsParsing(unittest.TestCase):
         expected_results = copy.copy(EXPECTED_RESULTS[OUTPUT_DUMP_FILE_REGULAR])
         expected_results['keyboard-convert-vc'] = ('running', None, False)
         self._run_with_expected_result(expected_results)
+
+
+class TestInstallationUrlStructureProcessing(unittest.TestCase):
+    """Test processing of installation url event structure."""
+
+    DEFAULTS_FILE_TEMPLATE = """
+{kstest_url}
+{kstest_metalink}
+{kstest_mirrorlist}
+{kstest_ftp_url}
+{kstest_modular_url}
+    """
+
+    cases = [
+        (
+            InstallationUrlsStructure(
+                None,
+                x86_64={
+                    'installation_tree': "http://dl.fedoraproject.org/pub/fedora/linux/development/rawhide/Everything/x86_64/os/",
+                    'some_url': "http://some.url.org",
+                }
+            ),
+            (
+                # boot.iso URL
+                "http://dl.fedoraproject.org/pub/fedora/linux/development/rawhide/Everything/x86_64/os/images/boot.iso",
+                # content of the override defaults file
+                DEFAULTS_FILE_TEMPLATE.format(
+                    kstest_url="export KSTEST_URL=http://dl.fedoraproject.org/pub/fedora/linux/development/rawhide/Everything/x86_64/os/",
+                    kstest_metalink="",
+                    kstest_mirrorlist="",
+                    kstest_ftp_url="",
+                    kstest_modular_url="",
+                ),
+            ),
+        ),
+        # empty values should result in no file created
+        (
+            InstallationUrlsStructure(
+                None,
+                x86_64={
+                    'installation_tree': "",
+                    'modular_url': "",
+                }
+            ),
+            (
+                None,
+                None,
+            ),
+        ),
+        (
+            InstallationUrlsStructure(
+                None,
+                x86_64={
+                    'installation_tree': 'http://dl.fedoraproject.org/pub/fedora/linux/development/rawhide/Everything/$basearch/os/',
+                    'metalink': 'https://mirrors.fedoraproject.org/metalink?repo=fedora-$releasever&arch=$basearch',
+                    'mirrorlist': 'https://mirrors.fedoraproject.org/mirrorlist?repo=fedora-$releasever&arch=$basearch',
+                    'modular_url': 'http://dl.fedoraproject.org/pub/fedora/linux/development/rawhide/Modular/$basearch/os/',
+                    'ftp_url': 'ftp://ftp.tu-chemnitz.de/pub/linux/fedora/linux/development/rawhide/Everything/$basearch/os/',
+                }
+            ),
+            (
+                "http://dl.fedoraproject.org/pub/fedora/linux/development/rawhide/Everything/$basearch/os/images/boot.iso",
+                DEFAULTS_FILE_TEMPLATE.format(
+                    kstest_url="export KSTEST_URL=http://dl.fedoraproject.org/pub/fedora/linux/development/rawhide/Everything/$basearch/os/",
+                    kstest_metalink="export KSTEST_METALINK=https://mirrors.fedoraproject.org/metalink?repo=fedora-$releasever&arch=$basearch",
+                    kstest_mirrorlist="export KSTEST_MIRRORLIST=https://mirrors.fedoraproject.org/mirrorlist?repo=fedora-$releasever&arch=$basearch",
+                    kstest_ftp_url="export KSTEST_FTP_URL=ftp://ftp.tu-chemnitz.de/pub/linux/fedora/linux/development/rawhide/Everything/$basearch/os/",
+                    kstest_modular_url="export KSTEST_MODULAR_URL=http://dl.fedoraproject.org/pub/fedora/linux/development/rawhide/Modular/$basearch/os/",
+                ),
+            ),
+        ),
+        (
+            InstallationUrlsStructure(
+                None,
+                x86_64={},
+                aarch64={
+                    'installation_tree': "http://dl.fedoraproject.org/pub/fedora/linux/development/rawhide/Everything/aarch64/os/",
+                }
+            ),
+            (
+                None,
+                DEFAULTS_FILE_TEMPLATE.format(
+                    kstest_url="",
+                    kstest_metalink="",
+                    kstest_mirrorlist="",
+                    kstest_ftp_url="",
+                    kstest_modular_url="",
+                ),
+            ),
+        ),
+    ]
+
+    @classmethod
+    def setUpClass(cls):
+        cls.library = Library('./tests/test_library/kickstart-test/basic')
+        cls.settings = Settings(
+            cmdline_overrides={
+                'kickstart_test': {
+                    'runner_command': "echo containers/runner/launch",
+                    'kstest_local_repo': "/tmp/mockrepo",
+                },
+            },
+            environment={},
+            settings_locations=[],
+        )
+        cls.event = TestFakeInstallationUrlsEvent(cls.settings)
+
+    def setUp(self):
+        self.testRuns = TestRuns(self.library, self.event, self.settings)
+        self._ensure_file_exists(DUMMY_BOOT_ISO_URL[7:])
+
+    def _ensure_file_exists(self, path):
+        if not os.path.isfile(path):
+            with open(path, 'w'):
+                pass
+
+    def _check_result(self, workflow, urls, expected_result):
+        url, fpath = workflow.process_installation_urls(urls)
+        self.assertEqual(url, expected_result[0])
+
+        expected_defaults_content = expected_result[1]
+        if expected_defaults_content is None:
+            self.assertIsNone(fpath)
+        else:
+            content = ""
+            if fpath:
+                with open(fpath, "r") as f:
+                    content = f.read()
+                os.unlink(fpath)
+            self.assertEqual(dedent(expected_defaults_content.strip()), dedent(content.strip()))
+
+    def testWorkflowRun(self):
+        executed_workflows = set()
+        self.assertEqual(len(self.testRuns.caseRunConfigurations), 1)
+        workflow = self.testRuns.caseRunConfigurations[0].workflow
+        for installation_urls, expected_result in self.cases:
+            self._check_result(workflow, installation_urls, expected_result)
