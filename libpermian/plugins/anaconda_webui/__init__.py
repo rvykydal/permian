1c3d035e7fc374585cb07611eec8a7cc8f343b40
Zdenek Veleba <zveleba@redhat.com>
2023-03-06 10:58:07 +0100

    Move ExecutionContainer to separate file

--- libpermian/plugins/anaconda_webui/__init__.py
+++ libpermian/plugins/anaconda_webui/__init__.py
@@ -18,6 +18,7 @@ from libpermian.workflows.isolated import IsolatedWorkflow
 from libpermian.events.structures.base import BaseStructure
 from libpermian.plugins.compose import ComposeStructure
 from libpermian.exceptions import StructureConversionError
+from libpermian.plugins.anaconda_webui.execution_container import ExecutionContainer
 from flask import url_for
 import libpermian.webui.builtin
 
@@ -87,46 +88,6 @@ class InstallationSourceStructure(BaseStructure):
                     repos[base_repo_id][arch]['initrd'] = compose.composeinfo.initrd_path(base_repo_id, arch)
 
         return cls(settings, base_repo_id, repos)
-
-
-class ExecutionContainer():
-    """ Handles creation, removal of container image and running commands in it """
-    def __init__(self, logfile):
-        # Initialization of ExecutionContainer is not thread safe and is expected to be done only once in setup_lock
-        self.image_name = 'anaconda-webui'
-
-        img = subprocess.check_output(['podman', 'images', '-q', '-f', f'reference={self.image_name}']).decode()
-        if img == '':
-            containerfile = os.path.join(os.path.dirname(__file__), 'Containerfile')
-            subprocess.check_call(['podman', 'build', '-f', containerfile, '-t', self.image_name], stdout=logfile, stderr=subprocess.STDOUT)
-
-    def _podman_cmd(self, cmd, volume, env, cwd, volume_mode):
-        env_params = [ item for key, val in env.items() for item in ('-e', f'{key}={val}') ]
-        podman_cmd = ['podman', 'run', '--rm', '-it',
-                      '-v', f'{volume}:/root/workdir:{volume_mode}',
-                      '-w', cwd] + env_params + [self.image_name] + cmd
-
-        LOGGER.debug('Running: ' + repr(podman_cmd))
-        return podman_cmd
-
-    def exec(self, cmd, volume, env={}, cwd='/root/workdir', volume_mode='z', log_error=True):
-        # Workaround for https://github.com/containers/podman/issues/15789
-        # volume_mode should be 'O' but that doesn't work when workdir is in it
-        podman_cmd = self._podman_cmd(cmd, volume, env, cwd, volume_mode)
-
-        proc = subprocess.run(podman_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-
-        if proc.returncode != 0 and log_error:
-            LOGGER.error(proc.stdout.decode())
-
-        return proc
-
-    def popen(self, cmd, volume, env={}, cwd='/root/workdir', volume_mode='z', stdout=subprocess.PIPE):
-        podman_cmd = self._podman_cmd(cmd, volume, env, cwd, volume_mode)
-        return subprocess.Popen(podman_cmd, stdout=stdout, stderr=subprocess.STDOUT)
-
-    def remove_image(self):
-        subprocess.check_call(['podman', 'rmi', self.image_name])
 
 
 @api.workflows.register('anaconda-webui')
